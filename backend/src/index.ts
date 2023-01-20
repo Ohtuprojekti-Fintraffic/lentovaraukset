4971adc9477897b8e6d55bd08918a4efbe015f08
vaisest <4550061+vaisest@users.noreply.github.com>
2023-01-20 14:21:33 +0200

    apply eslint style

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,11 @@
-import express from "express";
+import express from 'express';
 
 const port = process.env.PORT || 8080;
 
 const app = express();
 
-app.get("/", async (_req: any, res: express.Response) => {
-  res.send("Hello World");
+app.get('/', async (_req: any, res: express.Response) => {
+  res.send('Hello World');
 });
 
-app.listen(port, () =>
-  console.log(`Server is running on port: ${port}`)
-);
+app.listen(port, () => console.log(`Server is running on port: ${port}`));
