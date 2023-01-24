06291395e36bc6e697e9d9445edf29765f6b78dc
Joona Kauppila <joona.kauppila@helsinki.fi>
2023-01-24 15:00:32 +0200

    add calendar component

--- frontend/src/pages/Calendar.tsx
+++ frontend/src/pages/Calendar.tsx
@@ -2,20 +2,21 @@ import React from 'react'
 import { useQuery } from 'react-query'
 import { sampleQuery } from '../queries/query'
 import QueryKeys from '../queries/queryKeys'
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import TimeGridPlugin from '@fullcalendar/timegrid'
 
 const Calendar = () => {
 
   const {data, isLoading, isError} = useQuery(QueryKeys.Sample, sampleQuery)
 
   return(
-    <div className='flex flex-col space-y-2'>
+    <div className='flex flex-col space-y-2 h-full'>
       <h1 className='text-3xl'>Calendar</h1>
-      {isError
-        ? <p className='text-base'>error fetching text</p>
-        : isLoading
-        ? <p className='text-base'>fetching text</p>
-        : <p className='text-base'>fetched text: {data}</p>
-      }
+      <FullCalendar
+        plugins={[ TimeGridPlugin ]}
+        initialView="timeGridWeek"
+        height={'100%'}
+      />
     </div>
   )
 }
