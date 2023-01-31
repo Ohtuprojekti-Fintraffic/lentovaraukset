32800c9675d9bac1a2cab9787df5b9756dc0ca4a
Joona Kauppila <joona.kauppila@helsinki.fi>
2023-01-31 09:13:48 +0200

    basic calendar event functionality

--- frontend/src/pages/Calendar.tsx
+++ frontend/src/pages/Calendar.tsx
@@ -8,16 +8,42 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import listPlugin from '@fullcalendar/list'
 
 import interactionPlugin from '@fullcalendar/interaction'
+import { useState } from 'preact/hooks'
 
 const Calendar = () => {
 
   const {data, isLoading, isError} = useQuery(QueryKeys.Sample, sampleQuery)
+
+  const [events, setEvents] = React.useState([
+    {
+      title: 'test',
+      start: '2023-01-31T10:00:00',
+      end: '2023-01-31T10:45:00',
+    }
+  ])
+
+  const handleEventClick = (clickData: any) => {
+    console.log(`CLICK: ${clickData.event}`)
+  }
+
+  const handleEventChange = (changeData: any) => {
+    console.log(`CHANGE: ${changeData.event}`)
+  }
+
+  const handleEventAdd = (addData: any) => {
+    console.log(`ADD: ${addData.event}`)
+  }
+
+  const handleEventRemove = (removeData: any) => {
+    console.log(`REMOVE: ${removeData.event}`)
+  }
 
   return(
     <div className='flex flex-col space-y-2 h-full w-full'>
       <h1 className='text-3xl'>Varauskalenteri</h1>
       <FullCalendar
         plugins={[ timeGridPlugin, dayGridPlugin, listPlugin, interactionPlugin]}
+        locale='fi'
         headerToolbar={{
           left: 'prev,next today',
           center: 'title',
@@ -25,7 +51,6 @@ const Calendar = () => {
         }}
         height='100%'
         initialView="timeGridWeek"
-        locale='fi'
         allDaySlot={false}
         nowIndicator={true}
         scrollTime='08:00:00'
@@ -34,9 +59,15 @@ const Calendar = () => {
         slotLabelInterval={{minutes: 30}}
         slotLabelFormat={{hour:'numeric', minute:'2-digit', hour12:false, meridiem:false}}
         selectable={true}
+        selectMirror={true}
         editable={true}
         eventColor='#000000'
         eventBackgroundColor='#000000'
+        eventClick={handleEventClick}
+        eventAdd={handleEventAdd}
+        eventChange={handleEventChange}
+        eventRemove={handleEventRemove}
+        events={events}
       />
     </div>
   )
