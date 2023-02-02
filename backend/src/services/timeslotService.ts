1d7a8afa7ea1bacf4bd11197f4b6400a6aeb9478
antonlep <antonleppa@gmail.com>
2023-02-02 13:04:37 +0200

    refactor backend into routes and services

---
+++ backend/src/services/timeslotService.ts
@@ -0,0 +1,14 @@
+import { Timeslot } from '../models';
+
+const deleteById = async (id: number): Promise<boolean> => {
+  const timeslot = await Timeslot.findByPk(id);
+  if (timeslot) {
+    timeslot.destroy();
+    return true;
+  }
+  return false;
+};
+
+export default {
+  deleteById,
+};
