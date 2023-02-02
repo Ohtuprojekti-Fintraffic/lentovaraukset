025cb09bf1b5b57644810ba1b7aafe8d189e39c7
vaisest <4550061+vaisest@users.noreply.github.com>
2023-02-02 10:46:58 +0200

    revise deploy.sh script a bit

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,19 @@
-set -euxo pipefail
+#!/usr/bin/env bash
+set -euo pipefail
 
-echo "Pulling changes from git..."
-git pull
+echo "Fetching remote"
+git fetch origin main
 
-echo "Building and starting Docker container"
+
+if [ "$(git rev-parse HEAD)" = "$(git rev-parse @{u})" ]; then
+    echo "Already up to date. Exiting"
+    exit
+fi
+
+echo "Merging new commits"
+git merge --ff-only
+
+echo "Rebuilding and restarting containers"
 docker-compose -f docker-compose.yml up --build -d
 
 echo "Done!"
