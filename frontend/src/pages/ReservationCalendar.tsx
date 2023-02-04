083d5b7879159d82f91c5515f390ac1b711ed4f8
Joona Kauppila <joona.kauppila@helsinki.fi>
2023-02-04 10:31:02 +0200

    revise calendar refreshing

--- frontend/src/pages/ReservationCalendar.tsx
+++ frontend/src/pages/ReservationCalendar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { QueryClient, useMutation, useQuery } from 'react-query';
 import FullCalendar from '@fullcalendar/react';
 import timeGridPlugin from '@fullcalendar/timegrid';
@@ -15,42 +15,50 @@ function ReservationCalendar() {
 
   const queryClient = new QueryClient()
 
-  const { data: reservations, isLoading, isError, refetch: refetchReservations } = useQuery<any[]>(QueryKeys.Reservations, getReservations);
-  
+  const { data: reservations, isLoading, isError, isRefetching, isSuccess, refetch: refetchReservations } = useQuery<any[]>(QueryKeys.Reservations, getReservations);
+
+  useEffect(() => {
+    if (isSuccess) {
+      refreshCalendar()
+    }
+  }, [isRefetching])
+
   const newReservation = useMutation(addReservation, {
     onSuccess: () => {
-      refreshCalendar()
+      queryClient.invalidateQueries(QueryKeys.Reservations)
+      refetchReservations()
     }
   })
 
   // When a reservation box is clicked
   const handleReservationClick = (clickData: any) => {
-    console.dir(clickData.event);
     // Open reservation info screen
     // Refresh calendar if changes were made
   };
 
   // When a reservation box is moved or resized
   const handleReservationChange = (changeData: any) => {
-    console.dir(changeData.event);
     // Open confirmation popup
+    confirm('are you sure?') ?
+    refetchReservations() :
+    refreshCalendar()
   };
 
   // When a new reservation is selected (dragged) in the calendar.
   const handleReservationDrop = (dropData: any) => {
-    console.dir(dropData);
     newReservation.mutateAsync({
       title: 'test',
       start: dropData.startStr,
       end: dropData.endStr,
     })
+    const calendar = calendarRef.current?.getApi()
+    calendar?.unselect()
   };
 
   const refreshCalendar = () => {
-    queryClient.invalidateQueries(QueryKeys.Reservations)
-    refetchReservations()
     const calendar = calendarRef.current?.getApi()
-    calendar?.unselect()
+    calendar?.removeAllEventSources()
+    calendar?.addEventSource(reservations!)
   }
 
   return (
@@ -79,6 +87,8 @@ function ReservationCalendar() {
         selectable
         selectMirror
         editable
+        eventResizableFromStart
+        eventStartEditable
         eventColor="#000000"
         eventBackgroundColor="#000000"
         eventClick={handleReservationClick}
