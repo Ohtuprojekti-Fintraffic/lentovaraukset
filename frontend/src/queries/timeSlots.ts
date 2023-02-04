8a4f2b1283471da621acd8e2e407ad97e1b50072
Joona Kauppila <joona.kauppila@helsinki.fi>
2023-02-04 13:38:47 +0200

    add timeslot calendar

--- frontend/src/queries/timeSlots.ts
+++ frontend/src/queries/timeSlots.ts
@@ -1,24 +1,23 @@
 let placehoderTimeSlots = [
   {
     id: '1',
-    start: '2023-01-31T8:00:00',
+    start: '2023-01-31T10:00:00',
     end: '2023-01-31T16:00:00',
     editable: true,
   },
   {
     id: '2',
-    start: '2023-02-1T8:00:00',
-    end: '2023-02-1T16:00:00',
+    start: '2023-02-01T10:00:00',
+    end: '2023-02-01T16:00:00',
     editable: true,
   },
   {
     id: '3',
-    start: '2023-02-2T8:00:00',
-    end: '2023-02-2T14:00:00',
+    start: '2023-02-02T10:00:00',
+    end: '2023-02-02T14:00:00',
     editable: true,
   },
 ];
-
 const getTimeSlots = async (): Promise<any[]> => (placehoderTimeSlots);
 
 const addTimeSlot = async (newTimeSlot: any): Promise<void> => {
@@ -30,7 +29,12 @@ const addTimeSlot = async (newTimeSlot: any): Promise<void> => {
 const modifyTimeSlot = async (timeSlot: any): Promise<void> => {
   placehoderTimeSlots[
     placehoderTimeSlots.findIndex(((element) => element.id === timeSlot.id))
-  ] = timeSlot;
+  ] = {
+    id: timeSlot.id,
+    start: timeSlot.start,
+    end: timeSlot.end,
+    editable: true,
+  };
 };
 
 export { getTimeSlots, addTimeSlot, modifyTimeSlot };
