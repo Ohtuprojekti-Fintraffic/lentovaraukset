83e4b3d236c08d84ab66f768ffb31fb41c69937d
Nuutti Nikkola <nuuttinikkola1@gmail.com>
2023-02-06 13:09:58 +0200

    Add api endpoint to get timeslots

--- backend/src/services/timeslotService.ts
+++ backend/src/services/timeslotService.ts
@@ -1,4 +1,26 @@
+import { Op } from 'sequelize';
 import { Timeslot } from '../models';
+
+const getInTimeRange = async (rangeStartTime: Date, rangeEndTime: Date) => {
+  const timeslots = await Timeslot.findAll({
+    where: {
+      [Op.or]: [{
+        startTime: {
+          [Op.between]: [rangeStartTime, rangeEndTime],
+        },
+      }, {
+        endTime: {
+          [Op.between]: [rangeStartTime, rangeEndTime],
+        },
+      }],
+    },
+  });
+
+  return timeslots.map((timeslot) => ({
+    start: timeslot.dataValues.startTime,
+    end: timeslot.dataValues.endTime
+  }));
+};
 
 const deleteById = async (id: number): Promise<boolean> => {
   const timeslot = await Timeslot.findByPk(id);
@@ -9,11 +31,12 @@ const deleteById = async (id: number): Promise<boolean> => {
   return false;
 };
 
-const updateById = async (id: number, timeslot: { starttime: Date, maxDuration: number }) => {
+const updateById = async (id: number, timeslot: { startTime: Date, endTime: Date, maxConcurrentFlights: number }) => {
   await Timeslot.update(timeslot, { where: { id } });
 };
 
 export default {
+  getInTimeRange,
   deleteById,
   updateById,
 };
