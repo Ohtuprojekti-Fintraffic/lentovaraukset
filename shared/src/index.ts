e66d0e26705f5833d7651366ad2402810d677c2a
Hamid Aebadi <hamidebadi1996@yahoo.com>
2023-02-10 17:58:18 +0200

    New types for reservation and Time frame

---
+++ shared/src/index.ts
@@ -0,0 +1,16 @@
+export interface ReservationEntry {
+    id: number;
+    startTime: Date;
+    endTime: Date;
+    user: string;
+    plateNumber: string;
+    phone: string;
+    email?: string;
+    info: string;
+}
+
+export interface TimeFrame {
+    id: number;
+    startTime: Date;
+    endTime: Date;
+}
