70ed63affd2816c9187768aba1a097edd2beced8
Hamid Aebadi <hamidebadi1996@yahoo.com>
2023-02-11 11:55:26 +0200

    Fix indentations

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,16 +1,16 @@
 export interface ReservationEntry {
-    id: number;
-    startTime: Date;
-    endTime: Date;
-    user: string;
-    plateNumber: string;
-    phone: string;
-    email?: string;
-    info: string;
+  id: number;
+  startTime: Date;
+  endTime: Date;
+  user: string;
+  plateNumber: string;
+  phone: string;
+  email?: string;
+  info: string;
 }
 
 export interface TimeFrame {
-    id: number;
-    startTime: Date;
-    endTime: Date;
+  id: number;
+  startTime: Date;
+  endTime: Date;
 }
