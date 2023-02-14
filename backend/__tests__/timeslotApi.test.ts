3dc942f7a700a03dffb8508f6edd013b833cede5
antonlep <antonleppa@gmail.com>
2023-02-14 21:42:04 +0200

    add tests to create and delete timeslot

--- backend/__tests__/timeslotApi.test.ts
+++ backend/__tests__/timeslotApi.test.ts
@@ -5,6 +5,12 @@ import { connectToDatabase, sequelize } from '../src/util/db';
 
 const api = request(app);
 
+const timeslot_data = [
+  {start: new Date('2023-02-02T08:00:00.000Z'), end: new Date('2023-02-02T10:00:00.000Z')},
+  {start: new Date('2023-02-02T14:00:00.000Z'), end: new Date('2023-02-02T16:00:00.000Z')},
+  {start: new Date('2023-02-02T16:00:00.000Z'), end: new Date('2023-02-02T18:00:00.000Z')},
+];
+
 beforeAll(async () => {
   await connectToDatabase();
 });
@@ -12,6 +18,7 @@ beforeAll(async () => {
 beforeEach(async () => {
   // wipe db before each test
   await sequelize.truncate({ cascade: true });
+  await Timeslot.bulkCreate(timeslot_data);
 });
 
 afterAll(async () => {
@@ -19,6 +26,23 @@ afterAll(async () => {
   await sequelize.close();
 });
 describe('Calls to api', () => {
+
+  test('can create a timeslot', async () => {
+    await api.post('/api/timeslots/')
+      .set('Content-type', 'application/json')
+      .send({ start: '2023-03-03T12:00:00.000Z', end: '2023-03-03T14:00:00.000Z' });
+
+    const createdTimeslot: Timeslot | null = await Timeslot.findOne(
+      { where: { start: new Date('2023-03-03T12:00:00.000Z') } },
+    );
+    const numberOfTimeslots: Number = await Timeslot.count();
+
+    expect(numberOfTimeslots).toEqual(timeslot_data.length + 1)
+    expect(createdTimeslot).toBeDefined();
+    expect(createdTimeslot?.dataValues.start).toEqual(new Date('2023-03-03T12:00:00.000Z'));
+    expect(createdTimeslot?.dataValues.end).toEqual(new Date('2023-03-03T14:00:00.000Z'));
+  });
+
   test('can edit a timeslot', async () => {
     const createdSlot: Timeslot = await Timeslot.create({ start: '2023-01-01T12:00:00.000Z', end: '2023-01-01T13:00:00.000Z' });
 
@@ -33,5 +57,17 @@ describe('Calls to api', () => {
     expect(updatedSlot).toBeDefined();
     expect(updatedSlot?.dataValues.start).toEqual(new Date('2023-01-02T12:00:00.000Z'));
     expect(updatedSlot?.dataValues.end).toEqual(new Date('2023-01-02T14:00:00.000Z'));
+  });
+
+  test('can delete a timeslot', async () => {
+
+    const createdSlot: Timeslot | null = await Timeslot.findOne({});
+    await api.delete(`/api/timeslots/${createdSlot?.dataValues.id}`)
+
+    const deletedSlot: Timeslot | null = await Timeslot.findByPk(createdSlot?.dataValues.id);
+    const numberOfTimeslots: Number = await Timeslot.count();
+
+    expect(numberOfTimeslots).toEqual(timeslot_data.length - 1);
+    expect(deletedSlot).toEqual(null)
   });
 });
