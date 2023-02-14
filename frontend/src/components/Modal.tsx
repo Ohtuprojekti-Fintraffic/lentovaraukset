11bd9cfc311442a0672a08ce4446522c24454351
Joona Kauppila <joona.kauppila@helsinki.fi>
2023-02-14 11:31:28 +0200

    modal layout and close on background click

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -2,17 +2,23 @@ import React from "react"
 
 type Props = {
     show: boolean,
-    handleClose: () => void,
-    children: React.ReactNode
+    handleClose: any,
+    children?: React.ReactNode
 }
 
 const Modal: React.FC<Props> = ({ show, handleClose, children} ) => {
 
-    const showClass = show ? "" : ""
+    const showClass = show ? "flex" : "hidden"
 
     return (
-        <div className={`h-full w-full p-5 ${showClass}`}>
-            {children}
+        <div 
+            className={`${showClass} flex-col fixed left-0 top-0 w-screen h-screen bg-black bg-opacity-40 justify-center items-center z-50`}
+            onClick={() => handleClose(false)}
+        >
+            <div className='w-1/2 max-w-md h-fit bg-white p-6'>
+                <h1>test</h1>
+                {children}
+            </div>
         </div>
     )
 }
