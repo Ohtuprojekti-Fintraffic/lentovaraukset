263f6f35ab995c24611be008504ef608f9419024
Hamid Aebadi <hamidebadi1996@yahoo.com>
2023-02-19 07:37:14 +0200

    Add calendar event type

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -19,3 +19,12 @@ export interface ReservationStatus {
   availableSlots: (TimeslotEntry & { freeSlotsAmount: number })[];
   reservedSlots: ReservationEntry[];
 }
+
+export interface CalendarEvent {
+  id: number,
+  title: string,
+  start: Date,
+  end: Date,
+  user: string,
+  info: string
+}
