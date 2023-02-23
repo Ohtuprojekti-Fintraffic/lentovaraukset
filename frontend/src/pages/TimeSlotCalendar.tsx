9797fbb9b1bec875923642d5fe27970f33284636
antonlep <antonleppa@gmail.com>
2023-02-23 20:13:51 +0200

    make timeslots transparent and show reservations in background

--- frontend/src/pages/TimeSlotCalendar.tsx
+++ frontend/src/pages/TimeSlotCalendar.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 
 import Calendar from '../components/Calendar';
 import {
+  getReservations,
+} from '../queries/reservations';
+import {
   getTimeSlots, modifyTimeSlot, addTimeSlot, deleteTimeslot,
 } from '../queries/timeSlots';
 
@@ -14,7 +17,28 @@ function TimeSlotCalendar() {
   ) => {
     try {
       const timeslots = await getTimeSlots(start, end);
-      successCallback(timeslots);
+      const timeslotsMapped = timeslots.map((timeslot) => ({
+        ...timeslot, color: '#84cc1680',
+      }));
+      successCallback(timeslotsMapped);
+    } catch (error) {
+      failureCallback(error as Error);
+    }
+  };
+
+  const reservationsSourceFn: EventSourceFunc = async (
+    { start, end },
+    successCallback,
+    failureCallback,
+  ) => {
+    try {
+      const reservations = await getReservations(start, end);
+
+      const reservationsMapped = reservations.map((reservation) => ({
+        ...reservation, display: 'background', color: '#000000',
+      }));
+
+      successCallback(reservationsMapped);
     } catch (error) {
       failureCallback(error as Error);
     }
@@ -31,7 +55,7 @@ function TimeSlotCalendar() {
     <div className="flex flex-col space-y-2 h-full w-full">
       <h1 className="text-3xl">Vapaat varausikkunat</h1>
       <Calendar
-        eventSources={[timeSlotsSourceFn]}
+        eventSources={[timeSlotsSourceFn, reservationsSourceFn]}
         addEventFn={addTimeSlot}
         modifyEventFn={modifyTimeSlot}
         clickEventFn={clickEventFn}
