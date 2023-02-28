45f8848158eeccf3e80f3f8a795022d07f24934e
Oskari Heinonen <oskari.l.heinonen@helsinki.fi>
2023-02-28 12:28:28 +0200

    log complete original error

--- backend/src/util/middleware.ts
+++ backend/src/util/middleware.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Something went wrong';
-  console.log(err.statusCode, message);
+  console.log(err);
   res.status(statusCode).json({
     error: {
       message,
