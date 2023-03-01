665b0357f309880ecf960942482254599b6fbbc8
Nuutti Nikkola <nuuttinikkola1@gmail.com>
2023-03-01 15:32:02 +0200

    Add test airfield to db on startup

--- backend/src/util/db.ts
+++ backend/src/util/db.ts
@@ -1,10 +1,22 @@
 import { Sequelize } from 'sequelize';
 import { newDb } from 'pg-mem';
 import { DATABASE_URL } from './config';
+// eslint-disable-next-line import/no-cycle
+import { Airfield } from '../models';
 
 if (process.env.NODE_ENV === 'test') {
   console.log('creating an in-memory test database');
 }
+
+const createTestAirfield = () => {
+  // TODO: Remove this when we have a proper admin interface for creating airfields
+  Airfield.upsert({
+    id: 1,
+    name: 'Test Airfield',
+    maxConcurrentFlights: 3,
+    eventGranularityMinutes: 20,
+  });
+};
 
 const sequelize = process.env.NODE_ENV === 'test'
   ? new Sequelize({ dialect: 'postgres', dialectModule: newDb().adapters.createPg(), logging: false })
@@ -15,6 +27,7 @@ const connectToDatabase = async () => {
     await sequelize.sync({ alter: true });
     await sequelize.authenticate();
     console.log('database connected');
+    createTestAirfield();
   } catch (err) {
     console.log('connecting database failed. error:', err);
     return process.exit(1);
