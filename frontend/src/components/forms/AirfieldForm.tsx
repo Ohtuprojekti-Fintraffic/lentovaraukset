a3843c5d24307d181745f6173d42f140f1cf57cb
antonlep <antonleppa@gmail.com>
2023-03-03 19:21:32 +0200

    remove unnecessary files and variables

--- frontend/src/components/forms/AirfieldForm.tsx
+++ frontend/src/components/forms/AirfieldForm.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { AirfieldEntry } from '@lentovaraukset/shared/src';
 import Button from '../Button';
-imprt InputField from '../InputField';
+import InputField from '../InputField';
 
 type FormProps = {
   airfield: AirfieldEntry;
@@ -12,7 +12,7 @@ type Inputs = {
   maxFlights: string;
 };
 
-export function AirfieldForm(
+function AirfieldForm(
   { airfield }: FormProps,
 ) {
   const {
@@ -20,11 +20,10 @@ export function AirfieldForm(
   } = useForm<Inputs>({
     defaultValues: {
       maxFlights: airfield.maxConcurrentFlights.toString(),
-    }
+    },
   });
-  const textFieldStyle = 'border border-black rounded p-1 ml-4';
   const labelStyle = 'flex flex-row justify-between items-center w-full';
-  const {ref, ...rest} = register("maxFlights")
+  const { ref, ...rest } = register('maxFlights');
   const onSubmit: SubmitHandler<Inputs> = (data: Inputs) => console.log(data);
   return (
     <div>
@@ -54,3 +53,5 @@ export function AirfieldForm(
     </div>
   );
 }
+
+export default AirfieldForm;
