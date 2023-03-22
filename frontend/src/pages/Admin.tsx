10e31c065715db4a8c9e12348c389980a8cea8e6
antonlep <antonleppa@gmail.com>
2023-03-22 16:53:55 +0200

    add tests for adding airfield

--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -5,7 +5,7 @@ import { createAirfieldMutation } from '../queries/airfields';
 
 function Admin() {
   return (
-    <div className="flex flex-col space-y-2">
+    <div className="flex flex-col">
       <AdminForm title="Globaalit asetukset" />
       <AirfieldForm title="Lisää lentokenttä" airfield={undefined} airfieldMutation={createAirfieldMutation} />
     </div>
