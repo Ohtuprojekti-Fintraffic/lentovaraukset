e1505e515e963024282a011412200925e06e5a8f
Kimmo Muli <kimmomuli@gmail.com>
2023-03-24 12:09:33 +0200

    added WeekInDays interface in timeslotquery

--- frontend/src/queries/timeSlots.ts
+++ frontend/src/queries/timeSlots.ts
@@ -1,4 +1,4 @@
-import { TimeslotEntry } from '@lentovaraukset/shared/src';
+import { TimeslotEntry, WeekInDays } from '@lentovaraukset/shared/src';
 import { errorIfNotOk } from './util';
 
 const getTimeSlots = async (from: Date, until: Date): Promise<TimeslotEntry[]> => {
@@ -23,15 +23,7 @@ const modifyTimeSlot = async (
   period?: {
     end: Date,
     name: string,
-    days: {
-      monday: boolean,
-      tuesday: boolean,
-      wednesday: boolean,
-      thursday: boolean,
-      friday: boolean,
-      saturday: boolean,
-      sunday: boolean,
-    }
+    days: WeekInDays
   },
 ): Promise<void> => {
   const modifiedTimeSlot = {
