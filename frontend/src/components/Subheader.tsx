e30060774a0f60c56c7f13b1078b1e872a2736be
Kimmo Muli <kimmomuli@gmail.com>
2023-03-31 15:16:19 +0300

    used tailwind in subheader

--- frontend/src/components/Subheader.tsx
+++ frontend/src/components/Subheader.tsx
@@ -4,7 +4,7 @@ interface SubheaderProps {
   children: string;
   className?: string;
 }
-const subheaderBaseClass = 'font-publicSans font-medium text-base text-black align-left align-top leading-[137.5%]';
+const subheaderBaseClass = 'font-sans text-ft-text-1000 text-left text-top leading-[137.5%]';
 
 function Subheader({ children, className }: SubheaderProps) {
   return (
