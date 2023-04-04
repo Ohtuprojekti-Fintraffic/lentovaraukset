c8ba3654fba6690b7b936b596744dba1d6c73cf4
vaisest <4550061+vaisest@users.noreply.github.com>
2023-04-04 14:41:20 +0300

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,11 @@ fi
 echo "Found new commits. Merging"
 git merge --ff-only
 
+echo "Stopping existing containers and removing images and volumes"
+docker-compose -f docker-compose.yml down
+docker image rm lentovaraukset_backend lentovaraukset_frontend
+docker volume rm lentovaraukset_data
+
 echo "Rebuilding and restarting containers"
 docker-compose -f docker-compose.yml build --build-arg COMMIT_HASH=$(git rev-parse --short HEAD)
 docker-compose -f docker-compose.yml up -d
