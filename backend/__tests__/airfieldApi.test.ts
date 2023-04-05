c43aec4160f0fef3692868c7ead7fcd056356f20
antonlep <antonleppa@gmail.com>
2023-04-05 10:41:50 +0300

    airfield validation checks that code is not too short

--- backend/__tests__/airfieldApi.test.ts
+++ backend/__tests__/airfieldApi.test.ts
@@ -135,4 +135,30 @@ describe('Calls to api', () => {
     expect(res.body.error.message).toContain('Time must be multiple of 10');
     expect(originalAirfield).toMatchObject(airfields[0]);
   });
+
+  test('dont create an airfield if code too short', async () => {
+    const res = await api.post('/api/airfields/')
+      .set('Content-type', 'application/json')
+      .send({
+        code: 'EFH', name: 'Malmi', maxConcurrentFlights: 2, eventGranularityMinutes: 20,
+      });
+
+    const numberOfAirfields: Number = await Airfield.count();
+
+    expect(res.body.error.message).toContain('must be ICAO airport code');
+    expect(numberOfAirfields).toEqual(airfields.length);
+  });
+
+  test('dont create an airfield if code too long', async () => {
+    const res = await api.post('/api/airfields/')
+      .set('Content-type', 'application/json')
+      .send({
+        code: 'EFHKK', name: 'Malmi', maxConcurrentFlights: 2, eventGranularityMinutes: 20,
+      });
+
+    const numberOfAirfields: Number = await Airfield.count();
+
+    expect(res.body.error.message).toContain('must be ICAO airport code');
+    expect(numberOfAirfields).toEqual(airfields.length);
+  });
 });
