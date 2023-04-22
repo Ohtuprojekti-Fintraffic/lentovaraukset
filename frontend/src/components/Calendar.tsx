fde75ac82cc0ae0a2e7777bdb1b54666fa6c3b86
Joona Kauppila <joona.kauppila@helsinki.fi>
2023-04-22 04:12:30 +0300

    translations for calendar

--- frontend/src/components/Calendar.tsx
+++ frontend/src/components/Calendar.tsx
@@ -12,6 +12,7 @@ import type {
 import { EventImpl } from '@fullcalendar/core/internal';
 import { isTimeInPast, isTimeAtMostInFuture, isTimeFarEnoughInFuture } from '@lentovaraukset/shared/src/validation/validation';
 import { ConfigurationEntry } from '@lentovaraukset/shared/src';
+import { useTranslation } from 'react-i18next';
 import AlertContext from '../contexts/AlertContext';
 
 type CalendarProps = {
@@ -50,6 +51,9 @@ function Calendar({
   blocked = false,
 }: CalendarProps) {
   const calendarRef = forwardedCalendarRef || React.createRef();
+
+  const { t } = useTranslation();
+
   const { addNewAlert } = React.useContext(AlertContext);
   const [viewMode, setViewMode] = React.useState(window.innerWidth < 768 ? 'timeGridDay' : 'timeGridWeek');
 
@@ -94,24 +98,24 @@ function Calendar({
   const isTimeAllowed = (start: Date, end: Date, type?: string, ignoreStart?: boolean) => {
     if ((!ignoreStart && isTimeInPast(start)) || isTimeInPast(end)) {
       calendarRef.current?.getApi().unselect();
-      addNewAlert('Aikaa ei voi lisätä menneisyyteen', 'warning');
+      addNewAlert(t('calendar.notPast'), 'warning');
       return false;
     }
     const maxDaysInFuture = configuration ? configuration.maxDaysInFuture : 7;
     if (checkIfTimeInFuture && !isTimeAtMostInFuture(start, maxDaysInFuture)) {
       calendarRef.current?.getApi().unselect();
-      addNewAlert(`Aikaa ei voi lisätä yli ${maxDaysInFuture} päivän päähän`, 'warning');
+      addNewAlert(`${t('calendar.notTooFarFuture.begin')}${maxDaysInFuture}${t('calendar.notTooFarFuture.end')}$`, 'warning');
       return false;
     }
     const daysToStart = configuration ? configuration.daysToStart : 0;
     if (checkIfTimeInFuture && !isTimeFarEnoughInFuture(start, daysToStart)) {
       calendarRef.current?.getApi().unselect();
-      addNewAlert(`Aika pitää varata vähintään ${daysToStart} päivää etukäteen`, 'warning');
+      addNewAlert(`${t('calendar.notTooSoon.begin')}${daysToStart}${t('calendar.notTooSoon.end')}`, 'warning');
       return false;
     }
     if (timeIsConsecutive(start, end, type)) {
       calendarRef.current?.getApi().unselect();
-      addNewAlert('Ajat eivät voi olla peräkkäin', 'warning');
+      addNewAlert(t('calendar.notConsecutive'), 'warning');
       return false;
     }
     return true;
@@ -135,20 +139,20 @@ function Calendar({
     if (isReservation
       && oldEvent.start && isTimeInPast(oldEvent.start)) {
       changeData.revert();
-      addNewAlert('Alkanutta tai mennyttä varausta ei voi muokata', 'warning');
+      addNewAlert(t('calendar.cantModifyPast'), 'warning');
       return;
     }
 
     if (!isReservation && eventHasMoved
       && oldEvent.start && isTimeInPast(oldEvent.start)) {
       changeData.revert();
-      addNewAlert('Alkanutta tai mennyttä aikaikkunaa ei voi siirtää', 'warning');
+      addNewAlert(t('calendar.cantMovePast'), 'warning');
       return;
     }
 
     if (eventHasMoved && event.start && isTimeInPast(event.start)) {
       changeData.revert();
-      addNewAlert('Alkamisaikaa ei voi siirtää menneisyyteen', 'warning');
+      addNewAlert(t('calendar.cantMoveToPast'), 'warning');
       return;
     }
 
